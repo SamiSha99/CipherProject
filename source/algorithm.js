8e0fffa8d6b5b4ab78cd03b162171933be3b6de7
SamiSha99 <sami.shakkour@hotmail.com>
2019-05-08 19:53:56 +0300

    Updated deCipher funciton to work with multiple files.

--- source/algorithm.js
+++ source/algorithm.js
@@ -24,61 +24,56 @@ function cipher() {
   }
   setTimeout(function() {
     for (f = 0; f < input.files.length; f++) {
-
       data = new Uint8Array(fr[f].result);
       key = inputKey;
-      if (document.getElementById("encrypt").onclick == true) {
-        if (document.getElementById("vernam").checked == true) {
-          vernamCipher();
-        } else if (document.getElementById("columnar").checked == true) {
-          columnarCipher();
-        } else if (document.getElementById("mono").checked == true) {
-          document.getElementById("output").innerHTML = "Mono-Alphabetic is a work in progress, choose something else."
-        }
-      } else {
-        if (document.getElementById("vernam").checked == true) {
-          vernamCipher();
-        } else if (document.getElementById("columnar").checked == true) {
-          columnarDecipher();
-        } else if (document.getElementById("mono").checked == true) {
-          document.getElementById("output").innerHTML = "Mono-Alphabetic is a work in progress, choose something else."
-        }
+
+      if (document.getElementById("vernam").checked == true) {
+        vernamCipher();
+      } else if (document.getElementById("columnar").checked == true) {
+        columnarCipher();
+      } else if (document.getElementById("mono").checked == true) {
+        document.getElementById("output").innerHTML = "Mono-Alphabetic is a work in progress, choose something else."
       }
     }
   }, 1000);
 }
 
-
-
 function deCipher() {
   checkInput();
-
-  var fr = new FileReader();
-
-  consoleLog_fileName = input.files[0].name;
-  fileName = (input.files[0].name).replace(/ /g, "_");
-  fileType = input.files[0].type;
-  fileSize = input.files[0].size;
-
-  fr.readAsArrayBuffer(input.files[0]);
-
   document.getElementById("output").innerHTML = "<br><img src=\"./assets/images/Ripple-2s-200px.gif\">";
-
   var inputKey = document.getElementById("key").value.toString();
 
+  var fr = [];
+  file = [];
+  fileName = [];
+  fileType = [];
+  fileSize = [];
+
+  for (f = 0; f < input.files.length; f++) {
+
+    fr[f] = new FileReader(file);
+    fr[f].readAsArrayBuffer(input.files[f]);
+
+    consoleLog_fileName = input.files[f].name;
+    fileName[f] = (input.files[f].name).replace(/ /g, "_");
+    fileType[f] = input.files[f].type;
+    fileSize[f] = input.files[f].size;
+
+    console.log("Name: " + consoleLog_fileName + ", Type: " + fileType[f] + ", Size: " + (fileSize[f] / Math.pow(1024, 2)).toFixed(2) + "MB");
+    setTimeout(1000);
+  }
   setTimeout(function() {
+    for (f = 0; f < input.files.length; f++) {
+      data = new Uint8Array(fr[f].result);
+      key = inputKey;
 
-    console.log("Name: " + consoleLog_fileName + ", Type: " + fileType + ", Size: " + (fileSize / Math.pow(1024, 2)).toFixed(2) + "MB");
-
-    txt = new Uint8Array(fr.result); //works for Vernam Cipher
-    key = inputKey;
-
-    if (document.getElementById("vernam").checked == true) {
-      vernamCipher();
-    } else if (document.getElementById("columnar").checked == true) {
-      columnarDecipher();
-    } else if (document.getElementById("mono").checked == true) {
-      document.getElementById("output").innerHTML = "Mono-Alphabetic is a work in progress, choose something else."
+      if (document.getElementById("vernam").checked == true) {
+        vernamCipher();
+      } else if (document.getElementById("columnar").checked == true) {
+        columnarDecipher();
+      } else if (document.getElementById("mono").checked == true) {
+        document.getElementById("output").innerHTML = "Mono-Alphabetic is a work in progress, choose something else."
+      }
     }
   }, 1000);
 }
